90c923ef4e233cc4f0a7d83e70cfbc975bb3d4f1
Teresa Doyle Kovich <smit@groupon.com>
2016-04-25 09:47:30 -0500

    resub'd with edits; retaking course (April/May 2016)

--- plot1.R
+++ plot1.R
@@ -1,26 +1,30 @@
 # Download zip file ("exdata-data-household_power_consumption.zip") into the forked repo.
-# This will use the packages dplyr, tidyr, and lubridate; they must be installed for this to work.
+# This will use the packages tidyr, and lubridate. They must be installed for this to work.
+# Replace the first line with your personal working directory.
 
 setwd("c:/Users/Teresa/Documents/GitHub/datasciencecoursera/ExData_Plotting1")
-unzip("exdata-data-household_power_consumption.zip") -> data
-read.table(data[1], header=T) -> household
-require(dplyr)
-tbl_df(household)
-require(tidyr)
-separate(household, col = Date.Time.Global_active_power.Global_reactive_power.Voltage.Global_intensity.Sub_metering_1.Sub_metering_2.Sub_metering_3, into = c("Date", "Time","Global_active_power","Global_reactive_power","Voltage","Global_intensity","Sub_metering_1","Sub_metering_2","Sub_metering_3"), sep = ";") -> household
 
-require(lubridate)
-dmy(household$Date) -> household$Date
+  require(tidyr)
+  require(lubridate)
 
-group_by(household, Date) -> household
-filter(household, Date == household$Date[67248]) -> Feb_1
-filter(household, Date == household$Date[68248]) -> Feb_2
-rbind(Feb_1, Feb_2) -> dataset
+  unzip("exdata-data-household_power_consumption.zip") -> data
+    read.table(data[1], header=T) -> household
+      separate(household, col = Date.Time.Global_active_power.Global_reactive_power.Voltage.Global_intensity.Sub_metering_1.Sub_metering_2.Sub_metering_3,
+              into = c("Date", "Time","Global_active_power","Global_reactive_power","Voltage","Global_intensity",
+              "Sub_metering_1","Sub_metering_2","Sub_metering_3"), sep = ";") -> household
+        dmy(household$Date) -> household$Date
+          household[household$Date == ymd("2007-02-01 UTC"),] -> Feb_1
+          household[household$Date == ymd("2007-02-02 UTC"),] -> Feb_2
+            rbind(Feb_1, Feb_2) -> dataset
 
 hist(as.numeric(dataset$Global_active_power), freq = T) -> histoR1
 
-
 png()
-png("plot1.png", width = 480, height = 480)
-plot(histoR1, col = "Red", main = "Global Active Power", xlab = "Global Active Power (kilowatts)")
+
+  png("plot1.png", width = 480, height = 480)
+    plot(histoR1,
+         col = "Red",
+         main = "Global Active Power",
+         xlab = "Global Active Power (kilowatts)")
+
 graphics.off()
--- plot3.R
+++ plot3.R
@@ -1,28 +1,48 @@
 # Download zip file ("exdata-data-household_power_consumption.zip") into the forked repo.
 # This will use the packages dplyr, tidyr, and lubridate; they must be installed for this to work.
+# Replace the first line with your personal working directory.
+
 
 setwd("c:/Users/Teresa/Documents/GitHub/datasciencecoursera/ExData_Plotting1")
-unzip("exdata-data-household_power_consumption.zip") -> data
-read.table(data[1], header=T) -> household
-require(dplyr)
-tbl_df(household)
-require(tidyr)
-separate(household, col = Date.Time.Global_active_power.Global_reactive_power.Voltage.Global_intensity.Sub_metering_1.Sub_metering_2.Sub_metering_3, into = c("Date", "Time","Global_active_power","Global_reactive_power","Voltage","Global_intensity","Sub_metering_1","Sub_metering_2","Sub_metering_3"), sep = ";") -> household
 
-require(lubridate)
-dmy(household$Date) -> household$Date
+  require(dplyr)
+  require(tidyr)
+  require(lubridate)
 
-group_by(household, Date) -> household
-filter(household, Date == household$Date[67248]) -> Feb_1
-mutate(Feb_1, dt = as.POSIXct(paste(Feb_1$Date, Feb_1$Time), format = "%Y-%m-%d %H:%M:%S")) -> Feb_1
-filter(household, Date == household$Date[68248]) -> Feb_2
-mutate(Feb_2, dt = as.POSIXct(paste(Feb_2$Date, Feb_2$Time), format = "%Y-%m-%d %H:%M:%S")) -> Feb_2
-rbind(Feb_1, Feb_2) -> dataset
+    unzip("exdata-data-household_power_consumption.zip") -> data
+      read.table(data[1], header=T) -> household
+        separate(household, col = Date.Time.Global_active_power.Global_reactive_power.Voltage.Global_intensity.Sub_metering_1.Sub_metering_2.Sub_metering_3,
+                into = c("Date", "Time","Global_active_power","Global_reactive_power","Voltage","Global_intensity",
+                "Sub_metering_1","Sub_metering_2","Sub_metering_3"), sep = ";") -> household
+          dmy(household$Date) -> household$Date
+            household[household$Date == ymd("2007-02-01 UTC"),] -> Feb_1
+            household[household$Date == ymd("2007-02-02 UTC"),] -> Feb_2
+              mutate(Feb_1, dt = as.POSIXct(paste(Feb_1$Date, Feb_1$Time), format = "%Y-%m-%d %H:%M:%S")) -> Feb_1
+              mutate(Feb_2, dt = as.POSIXct(paste(Feb_2$Date, Feb_2$Time), format = "%Y-%m-%d %H:%M:%S")) -> Feb_2
+                rbind(Feb_1, Feb_2) -> dataset
 
 png()
-png("plot3.png", width = 480, height = 480)
-plot(dataset$dt, as.numeric(dataset$Sub_metering_1), type = "l", col = "black", ylab = "Energy sub metering", xlab = "")
-lines(dataset$dt, as.numeric(dataset$Sub_metering_2), type = "l", col = "red", ylab = "Energy sub metering", xlab = "")
-lines(dataset$dt, as.numeric(dataset$Sub_metering_3), type = "l", col = "blue", ylab = "Energy sub metering", xlab = "")
-legend("topright", pch = "___", col = c("black", "red", "blue"), legend = c("Sub_metering_1","Sub_metering_2","Sub_metering_3"))
+  png("plot3.png", width = 480, height = 480)
+    plot(dataset$dt,
+         as.numeric(dataset$Sub_metering_1),
+         type = "l",
+         col = "black",
+         ylab = "Energy sub metering",
+         xlab = "")
+        lines(dataset$dt,
+              as.numeric(dataset$Sub_metering_2),
+              type = "l",
+              col = "red",
+              ylab = "Energy sub metering",
+              xlab = "")
+        lines(dataset$dt,
+              as.numeric(dataset$Sub_metering_3),
+              type = "l",
+              col = "blue",
+              ylab = "Energy sub metering",
+              xlab = "")
+    legend("topright",
+           pch = "___",
+           col = c("black", "red", "blue"),
+           legend = c("Sub_metering_1","Sub_metering_2","Sub_metering_3"))
 graphics.off()
